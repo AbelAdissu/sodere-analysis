d998fc996eda4c511e086252e605555257c9025f
Abel Adissu <108996029+AbelAdissu@users.noreply.github.com>
2024-08-12 11:35:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #config.py
 
-api_id="21916567"
-api_hash= "82c37dcf99375c7d4b9a8d9a12f3e159"
-phone_number= "+251991476542"
+api_id="********"
+api_hash= "********************************"
+phone_number= "**********"
